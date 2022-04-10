2529e73138ed1db5d5f883606772c12441793a1f
Sanchit Kumar <mr.sanchitkumar@gmail.com>
2022-04-10 16:41:13 +0800

    ordering of fields in admin

--- projects/admin.py
+++ projects/admin.py
@@ -4,9 +4,10 @@ from .models import Project
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
-	list_display = ("title", "summary",)
+	list_display = ("title", "summary", "created_date", "last_updated")
 	prepopulated_fields = {
 		"slug": ("title",)
 	}
+	ordering = ['-created_date']
 
 admin.site.register(Project, ProjectAdmin)
